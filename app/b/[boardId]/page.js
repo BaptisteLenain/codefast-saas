c89e705f512949d86ef8f13ffac43b47490bb022
Baptiste Lenain <baptiste.lenain@hotmail.fr>
2025-01-20 21:18:01 +0100

    Post to board

--- app/b/[boardId]/page.js
+++ app/b/[boardId]/page.js
@@ -2,30 +2,41 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import connectMongo from "@/libs/mongoose";
 import Board from "@/models/Board";
+import Post from "@/models/Post";
+import FormAddPost from "@/components/FormAddPost";
+import CardPost from "@/components/CardPost";
 
-const getBoard = async (boardId) => {
+const getData = async (boardId) => {
   await connectMongo();
   const board = await Board.findById(boardId);
+  const posts = await Post.find({ boardId }).sort({ createdAt: -1 });
 
   if (!board) {
     redirect("/");
   }
 
-  return board;
+  return {
+    board,
+    posts,
+  };
 };
-
 export default async function PublicFeedbackBoard({ params }) {
   const { boardId } = params;
-  const board = await getBoard(boardId);
+  const { board, posts } = await getData(boardId);
 
   return (
     <main className="bg-base-200 min-h-screen">
-      {/*HEADER*/}
-      <section className="bg-base-100">
-        <div className=" max-w-5xl mx-auto px-5 py-3 flex"></div>
+      <section className="max-w-5xl mx-auto p-5">
+        <h1 className="text-lg font-bold">{board.name}</h1>
       </section>
-
-      {board.name}
+      <section className="max-w-5xl mx-auto px-5 flex flex-col md:flex-row items-start gap-8 pb-12">
+        <FormAddPost boardId={boardId} />
+        <ul className="space-y-4 flex-grow">
+          {posts.map((post) => (
+            <CardPost key={post._id} post={post} />
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
--- app/dashboard/b/[boardId]/page.js
+++ app/dashboard/b/[boardId]/page.js
@@ -2,11 +2,13 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import connectMongo from "@/libs/mongoose";
 import Board from "@/models/Board";
+import Post from "@/models/Post";
+import CardPostAdmin from "@/components/CardPostAdmin";
 import CardBoardLink from "@/components/CardBoardLink";
 import { auth } from "@/auth";
 import ButtonDeleteBoard from "@/components/ButtonDeleteBoard";
 
-const getBoard = async (boardId) => {
+const getData = async (boardId) => {
   const session = await auth();
   await connectMongo();
   const board = await Board.findOne({
@@ -18,13 +20,18 @@ const getBoard = async (boardId) => {
     redirect("/dashboard");
   }
 
-  return board;
+  const posts = await Post.find({ boardId }).sort({ createdAt: -1 });
+
+  return {
+    board,
+    posts,
+  };
 };
 
 export default async function FeedbackBoard({ params }) {
   const { boardId } = params;
 
-  const board = await getBoard(boardId);
+  const { board, posts } = await getData(boardId);
 
   return (
     <main className="bg-base-200 min-h-screen">
@@ -49,10 +56,18 @@ export default async function FeedbackBoard({ params }) {
         </div>
       </section>
 
-      <section className="max-w-5xl mx-auto px-5 py-12 space-y-12">
-        <h1 className="font-extrabold text-xl mb-4">{board.name}</h1>
-        <CardBoardLink boardId={board._id.toString()} />
-        <ButtonDeleteBoard boardId={board._id.toString()} />
+      <section className="max-w-5xl mx-auto px-5 py-12 flex flex-col md:flex-row gap-12">
+        <div className="space-y-8">
+          <h1 className="font-extrabold text-xl mb-4">{board.name}</h1>
+          <CardBoardLink boardId={board._id.toString()} />
+          <ButtonDeleteBoard boardId={board._id.toString()} />
+        </div>
+
+        <ul className="space-y-4">
+          {posts.map((post) => (
+            <CardPostAdmin key={post._id} post={post} />
+          ))}
+        </ul>
       </section>
     </main>
   );
