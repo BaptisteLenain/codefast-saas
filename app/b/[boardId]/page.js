d13a00c6648c522c3ecce303faa9ef26ecbd7f92
Baptiste Lenain <baptiste.lenain@hotmail.fr>
2025-01-24 12:07:34 +0100

    feature vote

--- app/b/[boardId]/page.js
+++ app/b/[boardId]/page.js
@@ -8,7 +8,7 @@ import CardPost from "@/components/CardPost";
 const getData = async (boardId) => {
   await connectMongo();
   const board = await Board.findById(boardId);
-  const posts = await Post.find({ boardId }).sort({ createdAt: -1 });
+  const posts = await Post.find({ boardId }).sort({ votesCounter: -1 });
 
   if (!board) {
     redirect("/");
@@ -28,7 +28,7 @@ export default async function PublicFeedbackBoard({ params }) {
       <section className="max-w-5xl mx-auto p-5">
         <h1 className="text-lg font-bold">{board.name}</h1>
       </section>
-      <section className="max-w-5xl mx-auto px-5 flex flex-col md:flex-row items-start gap-8 pb-12">
+      <section className="max-w-5xl mx-auto px-5 flex flex-col md:flex-row md:items-start gap-8 pb-12">
         <FormAddPost boardId={boardId} />
         <ul className="space-y-4 flex-grow">
           {posts.map((post) => (
--- components/CardPost.js
+++ components/CardPost.js
@@ -1,15 +1,18 @@
-"use client";
+import ButtonVote from "./ButtonVote";
 
 const CardPost = ({ post }) => {
   return (
-    <li className="bg-base-100 rounded-3xl p-6 flex justify-between items-center">
+    <li className="bg-base-100 rounded-3xl p-6 flex justify-between items-start">
       <div>
-        <div className="font-bold mb-1">{post.title}</div>
+        <div className="font-bold mb-1 text-lg">{post.title}</div>
         <div className="opacity-80 leading-relaxed max-h-32 overflow-scroll">
           {post.description}
         </div>
       </div>
-      <button className="btn btn-square">!</button>
+      <ButtonVote
+        postId={post._id.toString()}
+        initialVotesCounter={post.votesCounter}
+      />
     </li>
   );
 };
